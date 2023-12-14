b32bd40bca6c2c16425b50e9e4fe5e7fd1521ae3
Chernikov23 <118567820+Chernikov23@users.noreply.github.com>
2023-12-14 22:33:36 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+gigachat_credentials = "NzhjZTZjZGUtODA0NC00NmFiLTk0N2ItNGJmYmVkYzAzZjk2OjEzOTMyNDJjLWI4ZjAtNDE5Zi1iNjcwLTBiNzAwNjc3ZDc0ZA=="
+telegram_token = "6938400197:AAEyAEeKvCKrIHe3uHdLE4r1h_UZRhCMKeM"
+
+securePSID = 'eAj47Xva8ZMDicwjn5H_42LOWb2F1hLZhB_qZedH1GHBix0GCu7zyw_LDrKEXW9iqch8Kw.'
+openai_key = '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'
+testToken = '6970556386:AAHE8fUfFi1sphrLCUJQShYXBE2LxyvSYQk'
+
+gemToken = '6890530874:AAGgOa3dKsnpqryyhlpncOXVPEhXhOvxC3Y'
+
+netApi = 'AIzaSyCu8d4eQCHgcsy87EoIMhhZpRsSUdo8Who'
+glav = '6369798758:AAGpt4xSWNplzgSnHkDYY23gEAnf_J0Z6RI'
